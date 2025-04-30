42a49705efe95fa0b885a524de266ad8bae76d52
jerootieno <otienojerome7@gmail.com>
2025-04-30 10:56:09 +0300

    Changes

--- header.h
+++ header.h
@@ -1,241 +1,107 @@
-#ifndef HEADER_H
-#define HEADER_H
+// To make sure you don't declare the function more than once by including the header multiple times.
+#ifndef header_H
+#define header_H
 
-#include <string>
-#include <vector>
-#include <chrono>
-#include <map>
-#include <unordered_map>
-#include <algorithm>
-#include <cstring>
-#include <fstream>
-#include <sstream>
-#include <iostream>
-#include <iomanip>
-#include <memory>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <pwd.h>
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_opengl3.h"
+#include <stdio.h>
 #include <dirent.h>
+#include <vector>
+#include <iostream>
 #include <cmath>
-#include <regex>
-#include <array>
-#include <set>
+// lib to read from file
+#include <fstream>
+// for the name of the computer and the logged in user
+#include <unistd.h>
+#include <limits.h>
+// this is for us to get the cpu information
+// mostly in unix system
+// not sure if it will work in windows
+#include <cpuid.h>
+// this is for the memory usage and other memory visualization
+// for linux gotta find a way for windows
+#include <sys/types.h>
+#include <sys/sysinfo.h>
+#include <sys/statvfs.h>
+// for time and date
+#include <ctime>
+// ifconfig ip addresses
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <map>
 
-// ImGui includes
-#include "imgui/lib/imgui.h"
-#include "imgui/lib/backend/imgui_impl_sdl.h"
-#include "imgui/lib/backend/imgui_impl_opengl3.h"
+using namespace std;
 
-// Forward declarations
-class SystemInfo;
-class MemoryInfo;
-class NetworkInfo;
-
-// Data structures
-struct CPUData {
-    float usagePercent = 0.0f;
-    std::vector<float> history;
-    bool paused = false;
-    int fps = 60;
-    float scale = 100.0f;
+struct CPUStats
+{
+    long long int user;
+    long long int nice;
+    long long int system;
+    long long int idle;
+    long long int iowait;
+    long long int irq;
+    long long int softirq;
+    long long int steal;
+    long long int guest;
+    long long int guestNice;
 };
 
-struct FanData {
-    bool enabled = false;
-    int speed = 0;
-    int level = 0;
-    std::vector<float> history;
-    bool paused = false;
-    int fps = 60;
-    float scale = 100.0f;
-};
-
-struct ThermalData {
-    float temperature = 0.0f;
-    std::vector<float> history;
-    bool paused = false;
-    int fps = 60;
-    float scale = 100.0f;
-};
-
-struct MemoryData {
-    unsigned long total = 0;
-    unsigned long used = 0;
-    unsigned long free = 0;
-    float usedPercent = 0.0f;
-};
-
-struct SwapData {
-    unsigned long total = 0;
-    unsigned long used = 0;
-    unsigned long free = 0;
-    float usedPercent = 0.0f;
-};
-
-struct DiskData {
-    unsigned long total = 0;
-    unsigned long used = 0;
-    unsigned long free = 0;
-    float usedPercent = 0.0f;
-};
-
-struct ProcessInfo {
+// processes `stat`
+struct Proc
+{
     int pid;
-    std::string name;
-    std::string state;
-    float cpuUsage;
-    float memoryUsage;
-    bool selected;
+    string name;
+    char state;
+    long long int vsize;
+    long long int rss;
+    long long int utime;
+    long long int stime;
 };
 
-struct NetworkInterface {
-    std::string name;
-    std::string ipv4;
-    
-    // RX stats
-    unsigned long rx_bytes;
-    unsigned long rx_packets;
-    unsigned long rx_errs;
-    unsigned long rx_drop;
-    unsigned long rx_fifo;
-    unsigned long rx_frame;
-    unsigned long rx_compressed;
-    unsigned long rx_multicast;
-    
-    // TX stats
-    unsigned long tx_bytes;
-    unsigned long tx_packets;
-    unsigned long tx_errs;
-    unsigned long tx_drop;
-    unsigned long tx_fifo;
-    unsigned long tx_colls;
-    unsigned long tx_carrier;
-    unsigned long tx_compressed;
-    
-    float rx_usage_percent;
-    float tx_usage_percent;
+struct IP4
+{
+    char *name;
+    char addressBuffer[INET_ADDRSTRLEN];
 };
 
-struct SystemTasks {
-    int running = 0;
-    int sleeping = 0;
-    int uninterruptible = 0;
-    int zombie = 0;
-    int traced_stopped = 0;
-    int interrupted = 0;
-    int total = 0;
+struct Networks
+{
+    vector<IP4> ip4s;
 };
 
-// System Info Class
-class SystemInfo {
-public:
-    SystemInfo();
-    ~SystemInfo();
-    
-    void update();
-    
-    // System info getters
-    std::string getOSType() const;
-    std::string getUser() const;
-    std::string getHostname() const;
-    std::string getCPUType() const;
-    SystemTasks getTaskStats() const;
-    
-    // CPU, Fan, and Thermal data
-    CPUData& getCPUData();
-    FanData& getFanData();
-    ThermalData& getThermalData();
-    
-    // Draw system monitor tab
-    void drawSystemMonitor();
-    
-private:
-    std::string osType;
-    std::string user;
-    std::string hostname;
-    std::string cpuType;
-    SystemTasks tasks;
-    
-    CPUData cpuData;
-    FanData fanData;
-    ThermalData thermalData;
-    
-    // CPU measurements
-    unsigned long long lastTotalUser = 0;
-    unsigned long long lastTotalUserLow = 0;
-    unsigned long long lastTotalSys = 0;
-    unsigned long long lastTotalIdle = 0;
-    
-    std::chrono::time_point<std::chrono::steady_clock> lastCpuUpdateTime;
-    std::chrono::time_point<std::chrono::steady_clock> lastFanUpdateTime;
-    std::chrono::time_point<std::chrono::steady_clock> lastThermalUpdateTime;
-    
-    // Private methods
-    void updateSystemInfo();
-    void updateCPUUsage();
-    void updateFanInfo();
-    void updateThermalInfo();
-    void updateTaskStats();
-    
-    void drawPerformanceGraph(const char* label, std::vector<float>& history, float currentValue, 
-                             bool& paused, int& fps, float& scale, const char* overlay);
+struct TX
+{
+    int bytes;
+    int packets;
+    int errs;
+    int drop;
+    int fifo;
+    int frame;
+    int compressed;
+    int multicast;
 };
 
-// Memory Info Class
-class MemoryInfo {
-public:
-    MemoryInfo();
-    ~MemoryInfo();
-    
-    void update();
-    std::vector<ProcessInfo>& getProcesses();
-    
-    MemoryData& getRAMData();
-    SwapData& getSwapData();
-    DiskData& getDiskData();
-    
-    // Draw memory monitor tab
-    void drawMemoryMonitor();
-    
-private:
-    MemoryData ram;
-    SwapData swap;
-    DiskData disk;
-    std::vector<ProcessInfo> processes;
-    std::string filterText;
-    
-    // Process CPU usage calculation
-    std::unordered_map<int, unsigned long long> lastProcessCpuTime;
-    std::unordered_map<int, std::chrono::time_point<std::chrono::steady_clock>> lastProcessUpdateTime;
-    
-    void updateMemoryInfo();
-    void updateSwapInfo();
-    void updateDiskInfo();
-    void updateProcesses();
-    
-    float calculateProcessCpuUsage(int pid, unsigned long long processCpuTime);
+struct RX
+{
+    int bytes;
+    int packets;
+    int errs;
+    int drop;
+    int fifo;
+    int colls;
+    int carrier;
+    int compressed;
 };
 
-// Network Info Class
-class NetworkInfo {
-public:
-    NetworkInfo();
-    ~NetworkInfo();
-    
-    void update();
-    std::vector<NetworkInterface>& getInterfaces();
-    
-    // Draw network monitor tab
-    void drawNetworkMonitor();
-    
-private:
-    std::vector<NetworkInterface> interfaces;
-    int currentTabIndex;
-    
-    void updateNetworkInterfaces();
-    std::string formatBytes(unsigned long bytes);
-};
+// student TODO : system stats
+string CPUinfo();
+const char *getOsName();
 
-#endif // HEADER_H
+// student TODO : memory and processes
+
+// student TODO : network
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,17 +1,96 @@
 #include "header.h"
-#include <SDL2/SDL.h>
-#include "imgui/lib/gl3w/GL/gl3w.h"
+#include <SDL.h>
 
-// Main application
-int main(int argc, char** argv) {
+/*
+NOTE : You are free to change the code as you wish, the main objective is to make the
+       application work and pass the audit.
+
+       It will be provided the main function with the following functions :
+
+       - `void systemWindow(const char *id, ImVec2 size, ImVec2 position)`
+            This function will draw the system window on your screen
+       - `void memoryProcessesWindow(const char *id, ImVec2 size, ImVec2 position)`
+            This function will draw the memory and processes window on your screen
+       - `void networkWindow(const char *id, ImVec2 size, ImVec2 position)`
+            This function will draw the network window on your screen
+*/
+
+// About Desktop OpenGL function loaders:
+//  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
+//  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
+//  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+#include <GL/gl3w.h> // Initialize with gl3wInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+#include <GL/glew.h> // Initialize with glewInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include <glad/glad.h> // Initialize with gladLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+#include <glad/gl.h> // Initialize with gladLoadGL(...) or gladLoaderLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+#define GLFW_INCLUDE_NONE      // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/Binding.h> // Initialize with glbinding::Binding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+#define GLFW_INCLUDE_NONE        // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/glbinding.h> // Initialize with glbinding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#else
+#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#endif
+
+// systemWindow, display information for the system monitorization
+void systemWindow(const char *id, ImVec2 size, ImVec2 position)
+{
+    ImGui::Begin(id);
+    ImGui::SetWindowSize(id, size);
+    ImGui::SetWindowPos(id, position);
+
+    // student TODO : add code here for the system window
+
+    ImGui::End();
+}
+
+// memoryProcessesWindow, display information for the memory and processes information
+void memoryProcessesWindow(const char *id, ImVec2 size, ImVec2 position)
+{
+    ImGui::Begin(id);
+    ImGui::SetWindowSize(id, size);
+    ImGui::SetWindowPos(id, position);
+
+    // student TODO : add code here for the memory and process information
+
+    ImGui::End();
+}
+
+// network, display information network information
+void networkWindow(const char *id, ImVec2 size, ImVec2 position)
+{
+    ImGui::Begin(id);
+    ImGui::SetWindowSize(id, size);
+    ImGui::SetWindowPos(id, position);
+
+    // student TODO : add code here for the network information
+
+    ImGui::End();
+}
+
+// Main code
+int main(int, char **)
+{
     // Setup SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
+    // (Some versions of SDL before <2.0.10 appears to have performance/stalling issues on a minority of Windows systems,
+    // depending on whether SDL_INIT_GAMECONTROLLER is enabled or disabled.. updating to latest version of SDL is recommended!)
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
+    {
         printf("Error: %s\n", SDL_GetError());
         return -1;
     }
 
     // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
+    const char *glsl_version = "#version 130";
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
@@ -22,14 +101,31 @@ int main(int argc, char** argv) {
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window* window = SDL_CreateWindow("System Monitor", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    SDL_Window *window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, gl_context);
     SDL_GL_SetSwapInterval(1); // Enable vsync
 
     // Initialize OpenGL loader
+#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
     bool err = gl3wInit() != 0;
-    if (err) {
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+    bool err = glewInit() != GLEW_OK;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+    bool err = gladLoadGL() == 0;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+    bool err = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress) == 0; // glad2 recommend using the windowing library loader instead of the (optionally) bundled one.
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+    bool err = false;
+    glbinding::Binding::initialize();
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+    bool err = false;
+    glbinding::initialize([](const char *name) { return (glbinding::ProcAddress)SDL_GL_GetProcAddress(name); });
+#else
+    bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
+#endif
+    if (err)
+    {
         fprintf(stderr, "Failed to initialize OpenGL loader!\n");
         return 1;
     }
@@ -37,9 +133,8 @@ int main(int argc, char** argv) {
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    // render bindings
+    ImGuiIO &io = ImGui::GetIO();
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
@@ -48,17 +143,22 @@ int main(int argc, char** argv) {
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    // Load system monitors
-    SystemInfo systemInfo;
-    MemoryInfo memoryInfo;
-    NetworkInfo networkInfo;
+    // background color
+    // note : you are free to change the style of the application
+    ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 0.0f);
 
     // Main loop
     bool done = false;
-    while (!done) {
-        // Poll and handle events
+    while (!done)
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
         SDL_Event event;
-        while (SDL_PollEvent(&event)) {
+        while (SDL_PollEvent(&event))
+        {
             ImGui_ImplSDL2_ProcessEvent(&event);
             if (event.type == SDL_QUIT)
                 done = true;
@@ -71,54 +171,25 @@ int main(int argc, char** argv) {
         ImGui_ImplSDL2_NewFrame(window);
         ImGui::NewFrame();
 
-        // Update data
-        systemInfo.update();
-        memoryInfo.update();
-        networkInfo.update();
-
-        // Set up the main window to fill the screen
-        ImGui::SetNextWindowPos(ImVec2(0, 0));
-        ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
-        ImGui::Begin("System Monitor", nullptr, 
-                    ImGuiWindowFlags_NoTitleBar | 
-                    ImGuiWindowFlags_NoResize | 
-                    ImGuiWindowFlags_NoMove | 
-                    ImGuiWindowFlags_MenuBar);
-
-        // Menu bar
-        if (ImGui::BeginMenuBar()) {
-            if (ImGui::BeginMenu("File")) {
-                if (ImGui::MenuItem("Exit", "Alt+F4")) done = true;
-                ImGui::EndMenu();
-            }
-            ImGui::EndMenuBar();
+        {
+            ImVec2 mainDisplay = io.DisplaySize;
+            memoryProcessesWindow("== Memory and Processes ==",
+                                  ImVec2((mainDisplay.x / 2) - 20, (mainDisplay.y / 2) + 30),
+                                  ImVec2((mainDisplay.x / 2) + 10, 10));
+            // --------------------------------------
+            systemWindow("== System ==",
+                         ImVec2((mainDisplay.x / 2) - 10, (mainDisplay.y / 2) + 30),
+                         ImVec2(10, 10));
+            // --------------------------------------
+            networkWindow("== Network ==",
+                          ImVec2(mainDisplay.x - 20, (mainDisplay.y / 2) - 60),
+                          ImVec2(10, (mainDisplay.y / 2) + 50));
         }
-
-        // Main tabs for monitoring
-        ImGui::BeginTabBar("MonitorTabs");
-        
-        if (ImGui::BeginTabItem("System Monitor")) {
-            systemInfo.drawSystemMonitor();
-            ImGui::EndTabItem();
-        }
-        
-        if (ImGui::BeginTabItem("Memory & Processes")) {
-            memoryInfo.drawMemoryMonitor();
-            ImGui::EndTabItem();
-        }
-        
-        if (ImGui::BeginTabItem("Network")) {
-            networkInfo.drawNetworkMonitor();
-            ImGui::EndTabItem();
-        }
-
-        ImGui::EndTabBar();
-        ImGui::End();
 
         // Rendering
         ImGui::Render();
         glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
-        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         SDL_GL_SwapWindow(window);
